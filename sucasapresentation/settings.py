6b941cb220630949a5d0b2cb32e0c0f66e630800
kenmartey <kenmartey89@gmail.com>
2022-06-15 17:50:13 -0400

    Update settings

--- sucasapresentation/settings.py
+++ sucasapresentation/settings.py
@@ -152,4 +152,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # For Django 4
-CSRF_TRUSTED_ORIGINS = ['https://sucasa-presentation.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['sucasa-presentation.herokuapp.com']
