4b8ff6f6975098a2a8579c310baa4a9e765dc6a1
kenmartey <kenmartey89@gmail.com>
2022-06-16 15:15:31 -0400

    Added react site to settings

--- sucasapresentation/settings.py
+++ sucasapresentation/settings.py
@@ -153,5 +153,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # For Django 4
 CSRF_TRUSTED_ORIGINS = [
-    'https://sucasa-presentation.herokuapp.com/', 'http://localhost:3000']
+    'https://sucasa-presentation.herokuapp.com/', 'https://mellow-fudge-86498a.netlify.app/']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
