af82ab01ceb9207cfea4bb846f64cdae7362a26a
kenmartey <kenmartey89@gmail.com>
2022-06-16 20:54:22 -0400

    Changed settings

--- sucasapresentation/settings.py
+++ sucasapresentation/settings.py
@@ -158,15 +158,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_ALLOW_ALL = True
 CSRF_TRUSTED_ORIGINS = [
     'https://sucasa-presentation.herokuapp.com/',
-    # 'https://mellow-fudge-86498a.netlify.app',
-    # 'http://mellow-fudge-86498a.netlify.app',
-    # 'http://localhost:3000', 'http://127.0.0.0:3000'
+
 ]
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# CORS_ORIGIN_WHITELIST = (
-#     'http://localhost:3000',  # for localhost (REACT Default)
-#     'http://192.168.10.45:3000',  # for network
-#     'https://sucasa-presentation.herokuapp.com',
-#     'https://mellow-fudge-86498a.netlify.app',
-# )
+
 CORS_ALLOW_HEADERS = ['*']
--- sucasapresentation/urls.py
+++ sucasapresentation/urls.py
@@ -46,5 +46,7 @@ schema_view = get_schema_view(
 )
 urlpatterns += [
     path("api-docs/", schema_view.with_ui("swagger",
-         cache_timeout=0), name="api_docs")
+         cache_timeout=0), name="api_docs"),
+    path("", schema_view.with_ui("swagger",
+                                 cache_timeout=0), name="api_docs")
 ]
